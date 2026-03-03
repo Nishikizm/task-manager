56b53367429b6bfc84f0a07865da2231e8cba4d7
Nishikizm <sakuraiwase649@gmail.com>
2026-03-03 22:01:33 +0900

    Add Thymeleaf test page

--- src/main/java/io/github/nishikizm/taskmanager/controller/TaskController.java
+++ src/main/java/io/github/nishikizm/taskmanager/controller/TaskController.java
@@ -1,5 +1,15 @@
 package io.github.nishikizm.taskmanager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TaskController {
     
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("message", "Hello, Thymeleaf!");
+        return "test";
+    }
 }
