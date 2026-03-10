32db60f382b4a021de701930d70ef5d5ecdf59d8
Nishikizm <sakuraiwase649@gmail.com>
2026-03-11 00:09:28 +0900

    Add modal behavior

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,21 @@
 $(function() {
     // console.log("main.js loaded");
     $('.openModal').on('click', function(e) {
-        console.log("open clicked");
-        $('.modalOverlay').fadeIn();
+        $('#modalOverlay').fadeIn();
     });
+
     $('.closeModal').on('click', function(e) {
-        $('.modalOverlay').fadeOut();
+        console.log("close clicked");
+        $('#modalOverlay').fadeOut();
+    });
+
+    $(document).on('keydown', function(e) {
+        if(e.key === 'Escape') { $('#modalOverlay').fadeOut(); }
+    });
+
+    $('#modalOverlay').on('click', function(e) {
+        console.log("modalOverlay clicked");
+        const $back = $(e.target).closest('#modalForm');
+        if(!$back.length) { $('#modalOverlay').fadeOut(); }
     });
 });
